c590a3b496b4f7ab53b9c96dfd116bf813229933
Alexia Falcone <alexiafalcone1995@gmail.com>
2024-03-26 22:17:35 -0300

    Primer desafio terminado

--- primer_desafio/app.js
+++ primer_desafio/app.js
@@ -1,62 +1,78 @@
 class ProductManager {
     constructor() {
         this.products = []
+        this.idNumeber = 1
     }
     getProduct() {
         return this.products
     }
-    productsList(title, description, price, thumbnail, stock) {
-        const code = this.products.length + 1
-        const product = {
-            id: code,
-            title,
-            description,
-            price,
-            thumbnail,
-            stock,
+    addProduct(product, code) {
+        if (!this.productValid(product)) {
+            console.log("Debe completar todos los campos")
+            return
         }
-        this.products.push(product)
-    }
-    addProduct(product_id, new_title, new_description, new_price, new_image, new_stock) {
-        const productFound = this.products.find((product) => product.id === product_id)
+        const productFound = this.products.find((product) => product.code === code)
         if (productFound) {
             console.log("El producto ya se encuentra registrado.")
             return
         }
-        const newProduct = { ...this.productFound }
-        newProduct.id = this.products.length + 1
-        newProduct.title = new_title
-        newProduct.description = new_description
-        newProduct.price = new_price
-        newProduct.thumbnail = new_image
-        newProduct.stock = new_stock
+        
+        product.id = this.idNumeber++       
+        this.products.push(product)
 
-        this.products.push(newProduct)
-        console.log("El producto se agrego de forma correcta.")
     }
+    productValid(product) {
+        return (
+            product.title &&
+            product.description &&
+            product.price &&
+            product.thumbnail &&
+            product.stock &&
+            product.code 
+        )
+    }
+
     getProductById(product_id) {
         const singleProduct = this.products.find((product) => product.id === product_id)
-        if(singleProduct){
-            return singleProduct
-        } else{
+        if (!singleProduct) {
             console.log("Error, el producto no exite.")
+            return
         }
+        return singleProduct
+
     }
+ 
 }
 
 const productManager = new ProductManager()
 
-productManager.productsList("Blazer Monet", "Composición: Lino, disponible en 3 colores", 32000, "https://images.app.goo.gl/EbEPghSLdwjjma257", 15)
-productManager.productsList("Pantalon Monet", "Composición: Lino, disponible en 3 colores", 21000, "https://images.app.goo.gl/R4MLaCoMDuhrVxcF8", 15)
-productManager.productsList("Chaleco Monet", "Composición: Lino, disponible en 3 colores", 24500, "https://images.app.goo.gl/2YQ3Gt2xdh4fUjax6", 15)
-
-const products = productManager.getProduct()
-console.log(products)
-
-productManager.addProduct("Pollera", "Pollera tableada, disponible en 2 colores", 19200, 'https://images.app.goo.gl/QeCSiePaSDhYHvZd8', 10)
+productManager.addProduct({
+    title: "Blazer Monet",
+    description: "Composición: Lino, disponible en 3 colores", 
+    price: 32000, 
+    thumbnail: 'https://images.app.goo.gl/EbEPghSLdwjjma257', 
+    stock: 15, 
+    code: "A01",
+})
+productManager.addProduct({
+    title: "Pantalon Monet",
+    description: "Composición: Lino, disponible en 3 colores", 
+    price: 21000, 
+    thumbnail: 'https://images.app.goo.gl/R4MLaCoMDuhrVxcF8', 
+    stock: 15, 
+    code: "A02",
+})
+productManager.addProduct({
+    title: "Chaleco Monet",
+    description: "Composición: Lino, disponible en 3 colores", 
+    price: 24500, 
+    thumbnail: 'https://images.app.goo.gl/2YQ3Gt2xdh4fUjax6', 
+    stock: 10, 
+    code: "A03",
+})
 
 const newSeason = productManager.getProduct()
-console.log(newSeason)
+console.log("Todos los productos", newSeason)
 
-const bringSingleProduct = productManager.getProductById(2)
-console.log(bringSingleProduct)
+const bringSingleProduct = productManager.getProductById (2)
+ console.log("Producto único", bringSingleProduct)
